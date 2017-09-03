259475b2c24a45d8a2be8b913f660f2a10ed7198
Musa Sithole <musa.sithole@za.g4s.com>
2017-09-03 11:20:44 +0200

    Initial

---
+++ Program.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace G4SPulse
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((builderContext, config) =>
+                { 
+                    config.AddJsonFile("hosting.json", optional: true, reloadOnChange: true);
+                })
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
